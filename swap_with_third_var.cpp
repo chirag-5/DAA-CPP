ec607b77ba6f55d01c606e10f4cda84357b52bad
chirag narwal <142911984+chirag-5@users.noreply.github.com>
2025-08-05 09:10:21 +0530

    Add files via upload

---
+++ swap_with_third_var.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    // Swapping using third variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
